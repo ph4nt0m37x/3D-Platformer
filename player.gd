c9d35c4f9331fd73c94ea6e29ad6cf8760132f98
ph4nt0m <marija.ilievskax@gmail.com>
2025-06-29 23:05:20 +0200

    Added timer

--- player.gd
+++ player.gd
@@ -2,40 +2,59 @@ extends CharacterBody3D
 
 @export var speed = 5.0
 @export var acceleration = 4.0
-@export var jump_speed = 8.0
+@export var jump_speed = 20.0
 @export var rotation_speed = 12.0
 @export var mouse_sensitivity = 0.005
 
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 30.0
 var jumping = false
 
 var rescued := 0
 const TOTAL_ROBOTS := 10
 
+@onready var timer = $Timer
+@onready var timer_label = $TimerLabel
+var time_left := 30
+
+@onready var saved_label = $SavedLabel
 
 @onready var spring_arm = $SpringArm3D
 @onready var model = $player
 @onready var animation_tree = $player/AnimationTree
 @onready var animation_state = $player/AnimationTree.get("parameters/playback")
-
+@onready var game_over = $"../GameOverScreen"
+@onready var game_won = $"../GameWon"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Replace with function body.
-
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	timer_label.text = "%d s left  " % time_left
+	saved_label.text = "  %d/10 rescued" % rescued
+	timer.start()
+	game_over.hide()
+	game_won.hide()
+	
 
 func _physics_process(delta):
-	velocity.y = -gravity * delta
+	if not is_on_floor():
+		velocity.y = -gravity * delta * 0.5
+	else:
+		velocity.y = -gravity *delta * 2.0 
+	
+	#velocity.y = -gravity * delta
 	
 	get_movement_input(delta)
 
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+		animation_tree.set("parameters/StateMachine/current", "jump")
+		
 	move_and_slide()
+	
 	if velocity.length() > 1.0:
 		model.rotation.y = lerp_angle(model.rotation.y, spring_arm.rotation.y, rotation_speed * delta)
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = jump_speed
-		
 	
 func get_movement_input(delta):
 	var v = velocity.y
@@ -57,6 +76,19 @@ func _unhandled_input(event):
 	
 func collect_robots():
 	rescued += 1
+	saved_label.text = "  %d/10 rescued" % rescued
 	if (rescued == TOTAL_ROBOTS):
-		get_tree().change_scene_to_file("res://game_over_screen.tscn")
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
+		get_tree().change_scene_to_file("res://game_won.tscn")
+		
+
+
+func _on_timer_timeout() -> void:
+	time_left -= 1
+	timer_label.text = "%d s left  " % time_left
+	
+	if time_left <= 0:
+		if rescued < TOTAL_ROBOTS:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			get_tree().change_scene_to_file("res://game_over_screen.tscn")
